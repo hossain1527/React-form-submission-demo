1ab108bd99b821fce45f571a93a28710ab3e655f
hossain1527 <hossain.sci2015@gmail.com>
2020-10-22 22:28:48 +0100

    Updates

---
+++ precache-manifest.199cd4b6489c65cc269b72f594935076.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abfbe76a5364d3657aa10d3746bcea00",
+    "url": "/React-form-submission-demo/index.html"
+  },
+  {
+    "revision": "2486c997d16cc83c90cd",
+    "url": "/React-form-submission-demo/static/css/main.0cbde496.chunk.css"
+  },
+  {
+    "revision": "950922b2216e6743eeac",
+    "url": "/React-form-submission-demo/static/js/2.772f1904.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/React-form-submission-demo/static/js/2.772f1904.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2486c997d16cc83c90cd",
+    "url": "/React-form-submission-demo/static/js/main.8dbb009f.chunk.js"
+  },
+  {
+    "revision": "d17918775d5d10a677ea",
+    "url": "/React-form-submission-demo/static/js/runtime-main.015583f4.js"
+  }
+]);
